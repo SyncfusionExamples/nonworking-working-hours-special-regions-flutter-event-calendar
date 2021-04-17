6bd1c43864629b53e781b00b6f1692dc0bc6ecfd
Indumathi1195R <54974849+Indumathi1195R@users.noreply.github.com>
2021-04-17 20:49:19 +0530

    Update main.dart
    Sample updated.

--- lib/main.dart
+++ lib/main.dart
@@ -9,64 +9,26 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => ScheduleExample();
 }
 
-List<String> views = <String>[
-  'Day',
-  'Week',
-  'WorkWeek',
-  'Month',
-  'Timeline Day',
-  'Timeline Week',
-  'Timeline WorkWeek'
-];
-
 class ScheduleExample extends State<MyApp> {
-  CalendarView _calendarView;
-
-  @override
-  void initState() {
-    _calendarView = CalendarView.week;
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          leading: PopupMenuButton<String>(
-            icon: Icon(Icons.calendar_today),
-            itemBuilder: (BuildContext context) => views.map((String choice) {
-              return PopupMenuItem<String>(
-                value: choice,
-                child: Text(choice),
-              );
-            }).toList(),
-            onSelected: (String value) {
-              setState(() {
-                if (value == 'Day') {
-                  _calendarView = CalendarView.day;
-                } else if (value == 'Week') {
-                  _calendarView = CalendarView.week;
-                } else if (value == 'WorkWeek') {
-                  _calendarView = CalendarView.workWeek;
-                } else if (value == 'Month') {
-                  _calendarView = CalendarView.month;
-                } else if (value == 'Timeline Day') {
-                  _calendarView = CalendarView.timelineDay;
-                } else if (value == 'Timeline Week') {
-                  _calendarView = CalendarView.timelineWeek;
-                } else if (value == 'Timeline WorkWeek') {
-                  _calendarView = CalendarView.timelineWorkWeek;
-                }
-              });
-            },
+        body: SafeArea(
+          child: SfCalendar(
+            view: CalendarView.week,
+            allowedViews: [
+              CalendarView.day,
+              CalendarView.week,
+              CalendarView.workWeek,
+              CalendarView.timelineDay,
+              CalendarView.timelineWeek,
+              CalendarView.timelineWorkWeek,
+            ],
+            dataSource: getCalendarDataSource(),
+            specialRegions: _getTimeRegions(),
           ),
-        ),
-        body: SfCalendar(
-          view: _calendarView,
-          dataSource: getCalendarDataSource(),
-          specialRegions: _getTimeRegions(),
         ),
       ),
     );
@@ -111,26 +73,26 @@ class ScheduleExample extends State<MyApp> {
   List<TimeRegion> _getTimeRegions() {
     final List<TimeRegion> regions = <TimeRegion>[];
     regions.add(TimeRegion(
-        startTime: DateTime(2020, 5, 29, 00, 0, 0),
-        endTime: DateTime(2020, 5, 29, 09, 0, 0),
-        recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
-        color: Color(0xffbD3D3D3),
-        enablePointerInteraction: false,
-        ));
+      startTime: DateTime(2020, 5, 29, 00, 0, 0),
+      endTime: DateTime(2020, 5, 29, 09, 0, 0),
+      recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
+      color: Color(0xffbD3D3D3),
+      enablePointerInteraction: false,
+    ));
     regions.add(TimeRegion(
-        startTime: DateTime(2020, 5, 29, 18, 0, 0),
-        endTime: DateTime(2020, 5, 29, 24, 0, 0),
-        recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
-        color: Color(0xffbD3D3D3),
-        enablePointerInteraction: false,
-        ));
+      startTime: DateTime(2020, 5, 29, 18, 0, 0),
+      endTime: DateTime(2020, 5, 29, 24, 0, 0),
+      recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
+      color: Color(0xffbD3D3D3),
+      enablePointerInteraction: false,
+    ));
     regions.add(TimeRegion(
-        startTime: DateTime(2020, 5, 29, 09, 0, 0),
-        endTime: DateTime(2020, 5, 29, 18, 0, 0),
-        recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
-        color: Color(0xffb89cff0),
-        enablePointerInteraction: false,
-        ));
+      startTime: DateTime(2020, 5, 29, 09, 0, 0),
+      endTime: DateTime(2020, 5, 29, 18, 0, 0),
+      recurrenceRule: 'FREQ=DAILY;INTERVAL=1',
+      color: Color(0xffb89cff0),
+      enablePointerInteraction: false,
+    ));
 
     return regions;
   }
